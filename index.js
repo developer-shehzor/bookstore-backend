1fdb256510d55bbb30bde5ad65312e3fa59c6652
ShehzorMemon <Shezymemon0180@gmail.com>
2025-01-08 17:43:12 +0500

    Completed routes for the bookModel

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-import express from "express";
+import express, { request, response } from "express";
 import { mongoDBURL, PORT } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome to Bookstore Website')
 });
 
-
+app.use('/books', booksRoute);
 
 
 mongoose.connect(mongoDBURL).then(() => {
